783b5579da6ac947500e6e021ea44e9c7188dc37
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-09 16:51:33 +0600

    Added new component Diologs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css"
 import Diologs from "./components/Diologs/Diologs";
 import Header from "./components/Header/Header";
@@ -8,12 +9,18 @@ import Profile from "./components/Profile/Pfofile";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-      <Diologs />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/diologs" element={<Diologs />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
