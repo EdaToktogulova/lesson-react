ee612e1bc1b9121ccbd63a287fefb9fd312d93fd
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-09 12:30:04 +0600

    Add new component Post.js

---
+++ src/components/Profile/MyPosts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Post.module.css';
+
+const Post = () => {
+   return (
+      <div className={classes.item}>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1r6JLzg7t4O9HLayeMjzvfQ9sSX3xnlbeTg&usqp=CAU'></img>
+      post 1
+   </div>
+   )
+}
+
+export default Post;
