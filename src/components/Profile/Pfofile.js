220057e4c2fed502b7329b39e402df34728a6673
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-09 12:30:24 +0600

    Added  new components

---
+++ src/components/Profile/Pfofile.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
+import classes from './Profile.module.css';
+
+const Profile = () => {
+   return (
+      <div className={classes.content}>
+         <div>
+            <img src='https://tvbrics.com/upload/resize_cache/iblock/e30/800_500_0/e30f1f825b2bf1150b77c6181d671b30.jpg'></img>
+         </div>
+         <div>
+            ava + decc
+         </div>
+         <MyPosts />
+      </div>
+   )
+}
+
+export default Profile;
