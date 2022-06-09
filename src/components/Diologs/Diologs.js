11d64e7068e93d1c353d6ea50ddb0c0d44b7b3d9
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-09 16:54:04 +0600

    Document changes

--- src/components/Diologs/Diologs.js
+++ src/components/Diologs/Diologs.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './Diologs.module.css'
 
-const Diologs = () => {
+const Diologs = (props) => {
    return (
       <div>
-
+         Diologs
       </div>
    )
 }
--- src/components/Profile/Pfofile.js
+++ src/components/Profile/Pfofile.js
@@ -4,7 +4,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
    return (
-      <div className={classes.content}>
+      <div>
          <div>
             <img src='https://tvbrics.com/upload/resize_cache/iblock/e30/800_500_0/e30f1f825b2bf1150b77c6181d671b30.jpg'></img>
          </div>
