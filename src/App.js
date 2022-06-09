3ae36f421edb5f7ea4df29014f320cc842be3b64
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-06-09 16:07:37 +0600

    Add component Diologs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css"
+import Diologs from "./components/Diologs/Diologs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Pfofile";
@@ -11,6 +12,7 @@ const App = () => {
       <Header />
       <Navbar />
       <Profile />
+      <Diologs />
     </div>
   );
 }
---
+++ src/components/Diologs/Diologs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Diologs = () => {
+   return (
+      <div>
+
+      </div>
+   )
+}
+
+export default Diologs;
